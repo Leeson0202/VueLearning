dd798458a5da9938952b447ab253639a8c6341ca
Leeson0202 <asedrfa@163.com>
2022-07-27 15:22:34 +0800

    first commit

---
+++ 01-初识/11-单页文件组件.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>{{ message }}</p>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "hello"
+        }
+    }
+
+}
+</script>
+
+<style>
+</style>
